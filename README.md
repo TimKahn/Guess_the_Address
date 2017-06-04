9b7d8086d867132239a0987d9d0df35125565255
TimKahn <kahn.tim@gmail.com>
2017-06-04 13:10:44 -0600

    organized repo

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# galvanize-capstone
-Predicting addresses for AirDNA
+## Address Prediction for AirDNA
+Tim Kahn / 5.27.2017
+
+#### Overview:
+AirDNA has a robust model for predicting the AirBNB revenue potential of a property based on AirBNB rates/occupancy at comparable properties nearby.  As they expand into automated property valuation -- how much is a property actually worth, given its AirBNB revenue potential? -- they need address-specific information for AirBNB rentals in order to compare directly with market data, like MLS listings.
+
+#### Challenge / Goal:
+AirBNB publishes a good deal of information in its listings, but not the address -- you only get an address when you book a stay.  Maps on the AirBNB website show the general vicinity of the property as a circle, not as a point; the actual location of the property is randomized within the circle.  My goal is to 'predict' the addresses by comparing features of AirBNB listings to other data (e.g., from county recorder & tax assessor records), and provide a confidence measure for those predictions.
+
+#### Data:
+**Datasets:**
+* AirDNA's 'proprietary' data for Denver, scraped from AirBNB listings.
+* Similar data scraped from a competing service (Homeaway, VRBO) that includes reliable latitude/longitude.
+* County recorder data
+* Tax assessor data
+
+**Possible obstacle:**  
+One fundamental challenge is creating labeled training & test data.  The hope is that we can get labels by comparing AirBNB listings to other platforms' listings (Homeaway, VRBO) that reveal latitude/longitude.  Only listings with very close matches (on host name, description, etc.) will be used as training / test data, since this is actually a prediction too!  We believe that there is enough redundancy in listings across platforms for this to work.
+
+#### Ideas / methods:
+* Attempt to tackle each zip code independently.  Could be problematic for training, as classes will be severely imbalanced: matches may represent less than 1% of the available training data.
+* For each property within radius (400m?) of a property with known address, create a training point by comparing to the known property.  This will generate one 'match' training point and many 'non-match' training points.
+* Use logistic regression, SVMs, random forest, or boosting to classify correct matches (positive) vs. bad matches (negative).  Features could be boolean or differences:
+```
+"AirBNB Host Name == First Name on Deed"
+"AirBNB data shows swimming pool == Assessor data shows swimming pool"
+"Bedrooms on AirBNB listing - Bedrooms in Assessor Data"
+```
+
+* Extra credit: Bayesian methods.  If we correctly classify some portion of the properties in a zip code, is our posterior probability of classifying the remaining properties greater than the prior?
+* Extra credit: Extracting street numbers from images using a CNN.
+
+#### Presentation:
+MVP is a slide presentation and/or poster.  If time and results allow, a web app with a visualization of the model in a given zip code would be awesome.
+
+## Resources and References:
+
+#### Predicting address from property features/description
+* None found yet...
+
+#### Scraping:
+* https://ianlondon.github.io/blog/web-scraping-discovering-hidden-apis/
+* http://www.verginer.eu/blog/web-scraping-airbnb/
+* https://github.com/tomslee/airbnb-data-collection
+
+#### AirBNB-related news:
+* https://motherboard.vice.com/en_us/article/airbnbs-in-a-hot-legal-mess-with-new-york-and-its-not-going-away
---
+++ scrape/vrbo_scrape.py
@@ -0,0 +1,85 @@
+import requests
+import os
+import json
+import pandas as pd
+from pprint import pprint
+# from geopy.geocoders import GoogleV3
+import googlemaps
+import string
+import re
+
+gmaps = googlemaps.Client(key=os.environ['GMAPS_GEOLOCATOR'])
+
+def get_url(page_num):
+    parameters = dict(
+    sw_lat = '39.614431',
+    sw_long = '-105.109927',
+    ne_lat = '39.91424689999999',
+    ne_long = '-104.6002959',
+    page = page_num,
+    region = '2332'
+    )
+
+    url = "https://www.vrbo.com/ajax/map/results/vacation-rentals/@{sw_lat},{sw_long},{ne_lat},{ne_long},12z?swLat={sw_lat}&swLong={sw_long}&neLat={ne_lat}&neLong={ne_long}&zoom=12&page={page}&region={region}&searchTermContext=9b810827-b5cb-4e59-a1f0-553027e04694&searchTermUuid=9b810827-b5cb-4e59-a1f0-553027e04694&sleeps=1-plus&_=1496211019307".format(**parameters)
+
+    '''TEST URLs:
+    url1 = 'https://www.vrbo.com/ajax/map/results/vacation-rentals/@39.65237457610901,-105.04965595142579,39.826878836475025,-104.93086627857423,12z?swLat=39.65237457610901&swLong=-105.04965595142579&neLat=39.826878836475025&neLong=-104.93086627857423&zoom=12&page=1&region=2332&searchTermContext=9b810827-b5cb-4e59-a1f0-553027e04694&searchTermUuid=9b810827-b5cb-4e59-a1f0-553027e04694&sleeps=1-plus&_=1496211019307'
+
+    url2 = 'https://www.vrbo.com/ajax/map/results/vacation-rentals/@39.69280593436666,-105.01515201465821,39.78652608215945,-104.95575717823243,13z?swLat=39.69280593436666&swLong=-105.01515201465821&neLat=39.78652608215945&neLong=-104.95575717823243&zoom=13&page=2&region=2332&searchTermContext=9b810827-b5cb-4e59-a1f0-553027e04694&searchTermUuid=9b810827-b5cb-4e59-a1f0-553027e04694&sleeps=1-plus&_=1496510365866'
+
+    url3 = 'https://www.vrbo.com/ajax/map/results/vacation-rentals/@,,,,z?page=2&region=2332&searchTermContext=9b810827-b5cb-4e59-a1f0-553027e04694&searchTermUuid=9b810827-b5cb-4e59-a1f0-553027e04694&sleeps=1-plus&_=1496510660438'
+    '''
+
+    return url
+
+def get_data(url):
+    raw = requests.get(url).text
+    data = json.loads(raw)
+    return data['results']['hits']
+
+def get_street_address(split_address):
+    for elem in split_address:
+        street_number = re.sub(r'[^\w\s]', '', elem.split()[0])
+        if street_number.isnumeric():
+            return elem
+        else:
+            continue
+
+def reverse_geo(lat, lon):
+    print(lat, lon)
+    result = gmaps.reverse_geocode((lat, lon))[0]
+    full_address = result['formatted_address']
+    split_address = full_address.split(',')
+    street_address = get_street_address(split_address)
+    zipcode = split_address[-2].split()[-1]
+    gmaps_place_id = result['place_id']
+    return full_address, street_address, zipcode, gmaps_place_id
+
+def get_properties(url):
+    properties = []
+    for prop in get_data(url):
+        geo = prop['geoCode']
+        if geo['exact'] == True:
+            lat, lon = geo['latitude'], geo['longitude']
+            try:
+                full_address, street_address, zipcode, gmaps_place_id = reverse_geo(lat, lon)
+            except:
+                continue
+            properties.append([int(prop['listingNumber']), prop['headline'].strip(), full_address, street_address, zipcode, gmaps_place_id, lat, lon])
+    return properties
+
+def scrape_all():
+    all_properties = []
+    i = 1
+    while i <= 10:
+        try:
+            print('Attempting page {}'.format(i))
+            url = get_url(i)
+            all_properties.extend(get_properties(url))
+        except:
+            print('Page {} scrape failed.'.format(i))
+            break
+    return pd.DataFrame(all_properties, columns = ['listingNumber', 'headline', 'full address', 'street_address', 'zipcode', 'gmaps_place_id', 'lat', 'lon'])
+
+if __name__ == '__main__':
+    all_results = scrape_all()

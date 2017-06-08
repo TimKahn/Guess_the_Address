3ffc0fbc6beb11eddb82792300fd0f4a139ace97
TimKahn <kahn.tim@gmail.com>
2017-06-08 17:24:50 -0600

    looping through zipcodes

--- munge/attom.py
+++ munge/attom.py
@@ -22,15 +22,31 @@ def find_nearby(match, df_zip):
 
 def find_in_radius(match, df_near):
     radius = .5
-    lat = match['latitude']
     lon = match['longitude']
-    df_near['distance'] = df_near.apply(lambda row: get_distance(row, lat, lon), axis=1)
+    df_near['distance'] = df_near.apply(lambda row: get_distance(row, match['latitude'], match['longitude']), axis=1)
+    df_near['true_distance'] = df_near.apply(lambda row: get_distance(row, match['true_latitude'], match['true_longitude']), axis=1)
     df_radius = df_near[df_near['distance'] <= radius]
+    find_true_match(match, df_radius)
     return df_radius
 
+def find_true_match(match, df_radius):
+    print('\n****ACTUAL: {}'.format(match['street_address']))
+    df = df_radius[df_radius['true_distance'] < .002]
+    print('****MATCHES:\n{}'.format(df['PropertyAddressFull']))
+    return
+
+def find_all(matches_df, tax_df):
+    for z in matches_df.zipcode.unique():
+        matches_zip = matches_df[matches_df['zipcode'] == z]
+        df_zip = tax_df[tax_df['PropertyAddressZIP'] == z]
+        for i, match in matches_zip.iterrows():
+            df_near = find_nearby(match, df_zip)
+            df_radius = find_in_radius(match, df_near)
+            find_true_match(match, df_radius)
+    return
+
 if __name__ == '__main__':
-    matches = pd.read_csv('../data/matches_geo.csv')
-    df = pd.read_csv('../data/tax_assessor_denver.csv')
-    df_zip = df[(df['PropertyAddressZIP'] == 80209) & (df['CompanyFlag'] != 'Y')]
-    df_near = find_nearby(matches.loc[0], df_zip)
-    df_radius = find_in_radius(matches.loc[0], df_near)
+    matches_df = pd.read_csv('../data/matches_geo.csv')
+    tax_df = pd.read_csv('../data/tax_assessor_denver.csv')
+    tax_df = tax_df[tax_df['CompanyFlag'] != 'Y']
+    find_all(matches_df, tax_df)

a205549a3defc9beac4dc630fe699bd31655f59f
TimKahn <kahn.tim@gmail.com>
2017-06-13 08:50:10 -0600

    features

--- munge/featurize.py
+++ munge/featurize.py
@@ -19,7 +19,12 @@ Swimming pool in tax assessor data
 '''
 
 def get_features(df):
-
+    featurized_df = pd.DataFrame([])
+    featurized_df['attom_id'] = df['[ATTOM ID]']
+    featurized_df['airbnb_property_id'] = df['airbnb_property_id']
+    featurized_df['airbnb_host_id'] = df['airbnb_host_id']
+    featurized_df['first_name'] = df['first_name']
+    featurized_df['first_name2'] = df['first_name2']
 
 if __name__ == '__main__':
-    df = pd.read_csv('..data/merged.csv')
+    df = pd.read_csv('../data/merged.csv')

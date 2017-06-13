487a9b3336ceaba932ccc0b95118c8a1bec4da6a
TimKahn <kahn.tim@gmail.com>
2017-06-13 17:03:12 -0600

    fireplace

--- munge/featurize.py
+++ munge/featurize.py
@@ -2,12 +2,14 @@ import pandas as pd
 import geopy.distance
 
 '''FEATURES:
+zipcode
+neighbor_count: Number of 'neighbors' in radius
 lat_offset: Difference in latitude (AirBNB minus Tax Assessor)
 lon_offset: Difference in longitude (AirBNB minus Tax Assessor)
-neighbor_count: Number of 'neighbors' in radius
-Air Conditioning
-Heating
-Fireplace
+Air Conditioning -- not included; mostly NaN in attom, mostly True in airbnb
+Heating -- not included; assume the False listings in airbnb probably still have heating (it's Denver).
+fireplace_a: boolean -- airbnb indoor_fireplace == True
+fireplace_t: boolean -- tax assessor Fireplace code == 1.0
 Pets, elevator, pool, gym
 Similarity scores of AirBNB Host Name(s) and First Name on Deed etc. (each score is different predictor)
 Bedrooms on AirBNB listing
@@ -21,14 +23,19 @@ Has unit number
 
 def get_features(df):
     featurized_df = pd.DataFrame([])
+    featurized_df['MATCH'] = df.loc[:, 'MATCH']
     featurized_df['attom_id'] = df.loc[:, '[ATTOM ID]']
     featurized_df['airbnb_property_id'] = df.loc[:, 'airbnb_property_id']
     featurized_df['airbnb_host_id'] = df.loc[:, 'airbnb_host_id']
     featurized_df['first_name'] = df.loc[:, 'first_name']
     featurized_df['first_name2'] = df.loc[:, 'first_name2']
+    #predictors start here...
+    featurized_df['zipcode'] = df.loc[:, 'zipcode']
+    featurized_df['neighbor_count'] = df.loc[:, 'neighbor_count']
     featurized_df['lat_offset'] = df.loc[:, 'latitude'] - df.loc[:, 'PropertyLatitude']
     featurized_df['lon_offset'] = df.loc[:, 'longitude'] - df.loc[:, 'PropertyLongitude']
-    featurized_df['neighbor_count'] = df.loc[:, 'neighbor_count']
+    featurized_df['fireplace_a'] = df['indoor_fireplace'].apply(lambda x: 1 if x == True else 0)
+    featurized_df['fireplace_t'] = df['Fireplace'].apply(lambda x: 1 if x == 1.0 else 0)
     return featurized_df
 
 if __name__ == '__main__':

fa53e89313e42e064c9cd304574a9d62e751ad6b
TimKahn <kahn.tim@gmail.com>
2017-06-14 16:50:25 -0600

    ROCin out

--- model/blagging_model.py
+++ model/blagging_model.py
@@ -1,1 +1,69 @@
 from blagging import BlaggingClassifier
+import numpy as np
+from sklearn import cross_validation, metrics
+from sklearn.cross_validation import KFold, cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import auc, roc_auc_score, roc_curve, precision_recall_curve
+from scipy import interp
+import matplotlib.pyplot as plt
+import split
+
+def plot_ROC_curve(classifier, X, y, pos_label=1, n_folds=5):
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+    all_tpr = []
+    skf = StratifiedKFold(n_splits=n_folds, random_state=42, shuffle=True)
+    i = 1
+    for train, test in skf.split(X, y):
+        classifier.fit(X[train], y[train])
+        probas_ = classifier.predict_proba(X[test])
+        # Compute ROC curve and area under the curve
+        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1], pos_label=1)
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+        i += 1
+    plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Random')
+    mean_tpr /= n_folds
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    plt.plot(mean_fpr, mean_tpr, 'k--',
+         label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC curve')
+    plt.legend(loc="lower right")
+    plt.show()
+
+def plot_PR_curve(classifier, X, y, n_folds=5):
+    """
+    Plot precision/recall curve.
+    """
+    skf = StratifiedKFold(n_splits=n_folds, random_state=42, shuffle=True)
+    i = 1
+    for train, test in skf.split(X, y):
+        classifier.fit(X[train], y[train])
+        probas_ = classifier.predict_proba(X[test])
+        precision, recall, thresholds = precision_recall_curve(y[test], probas_[:,1], pos_label=1)
+        plt.plot(recall, precision, lw=1, label='PR fold %d' % (i,))
+        i += 1
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-recall curve')
+    plt.legend(loc="lower right")
+    plt.show()
+
+if __name__ == '__main__':
+    X_train, X_test, y_train, y_test = split.get_split()
+    bc = BlaggingClassifier(n_estimators=20)
+    # bc.fit(X_train, y_train)
+    # predictions = bc.predict(X_test)
+    # print(predictions.sum())
+    # print(predictions[np.where(y_test==1)].sum())
+    # plot_PR_curve(bc, X_train, y_train)
+    plot_ROC_curve(bc, X_train, y_train)
--- model/split.py
+++ model/split.py
@@ -5,8 +5,9 @@ def get_split():
     df = pd.read_csv('../data/featurized_data.csv')
     identifiers = df.loc[:, ['attom_id', 'airbnb_property_id', 'airbnb_host_id', 'first_name', 'first_name2', 'title']]
     df = df.drop(['attom_id', 'airbnb_property_id', 'airbnb_host_id', 'first_name', 'first_name2', 'title'], axis=1)
-    y = df.pop('MATCH')
-    X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=.33, random_state=42, stratify=y)
+    y = df.pop('MATCH').values
+    df = df.values
+    X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=.1, random_state=42, stratify=y)
     return X_train, X_test, y_train, y_test
 
 if __name__ == '__main__':
